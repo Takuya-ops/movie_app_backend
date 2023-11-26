1e8dd2bb52fe679952ef18bfd4dc20dead9cc564
Takuya-ops <hora2112.ipu@gmail.com>
2023-11-26 23:44:12 +0900

    comment add

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,7 +10,7 @@ import (
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
-	mux.Use(app.enableCORS)
+	mux.Use(app.enableCORS) //3000 to 8080
 	mux.Get("/", app.Home)
 	mux.Get("/movies", app.AllMovies)
 	return mux
